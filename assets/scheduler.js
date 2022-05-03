126be5ad74c369625451d4974ab1be704cc40d0b
alanam79 <halo2382001@gmail.com>
2022-05-03 19:04:01 -0400

    added for loop and if else statments for each hour

--- assets/scheduler.js
+++ assets/scheduler.js
@@ -1,3 +1,44 @@
-var displayCurrentDay = document.querySelector("#currentDay");
-var currentDay = moment();
-displayCurrentDay.textContent = currentDay.format('dddd, MMMM Do');
+$(document).ready(function () {
+  var displayCurrentDay = $("#currentDay");
+  var currentDay = moment();
+  displayCurrentDay.textContent = currentDay.format("dddd, MMMM Do");
+
+  $(".saveBtn").on("click", function () {
+    var textAreaValue = $(this).siblings(".description").val();
+    console.log(textAreaValue);
+    var timeBlock = $(this).parent().attr("id");
+    console.log("timeblock", timeBlock);
+
+    localStorage.setItem(timeBlock, textAreaValue);
+  });
+
+  function keepingTime() {
+    var currentTime = moment().hours();
+    console.log("hour of the day", currentTime);
+
+    $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id"));
+      console.log("timeBlockHour", timeBlockHour);
+
+      if (timeBlockHour < currentTime) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  keepingTime();
+
+  for (var i = 9; i < 18; i++) {
+    $("#" + i + ".description").val(localStorage.getItem(i));
+
+    // String Interpolation 
+    // console.log(`#${i}.description`).val(localStorage.getItem(i));  
+}
+});
